726a525843be36712c36e74258a9db01d731f8c6
SoulMonk <fsoulmonk@gmail.com>
2020-03-16 06:03:45 +0200

    main: added user_id to tables

--- main/migration/1584102611551-init.sql
+++ main/migration/1584102611551-init.sql
@@ -5,8 +5,11 @@ create table "training_session"
             primary key,
     title       varchar(64)  not null,
     description varchar(256) not null,
-    done        boolean      not null default false
+    done        boolean      not null default false,
+    user_id integer not null
 );
+create index training_session_user_id_index
+    on training_session (user_id);
 
 create table "training_plan"
 (
@@ -14,5 +17,9 @@ create table "training_plan"
         constraint training_session_tsid_fkey
             references training_session
             on update cascade,
-    priority integer
+    priority integer,
+    user_id integer not null
 );
+
+create index training_plan_user_id_index
+    on training_plan (user_id);
